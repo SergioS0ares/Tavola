1188ea93fbeaf82d3359972be5f5f9bbd52c3fea
Salahil <julioykawahata@gmail.com>
2025-02-17 21:56:01 -0300

    Initial Commit Esse será o primeiro commit diretamente da spring.initialzr dependencias essas como: Spring Boot DevTools Docker Compose Support Spring Web Spring Security Spring Data JPA WebSocket Oracle Vector Database Resillience4J Essas  são as dependencias selecionadas apenas para uma visão inicial do projeto, provavelmente elas irão ser alteradas, seja para remoção ou para adição de novas dependencias.

---
+++ TavolaSoftware/src/main/java/com/software/TavolaSoftware/TavolaSoftwaresApplication.java
@@ -0,0 +1,13 @@
+package com.software.TavolaSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavolaSoftwaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavolaSoftwaresApplication.class, args);
+	}
+
+}
---
+++ TavolaSoftware/src/test/java/com/software/TavolaSoftware/TavolaSoftwaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.TavolaSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavolaSoftwaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
