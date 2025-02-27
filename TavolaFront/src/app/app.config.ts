ff594a0f0ab3bb8224682b0607efd83416b7074c
SergioS0ares <sergyn200295@gmail.com>
2025-02-26 21:53:56 -0300

    Reboot do projeto front

--- TavolaFront/src/app/app.config.ts
+++ TavolaFront/src/app/app.config.ts
@@ -1,19 +1,9 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {authInterceptor} from "./interceptors/auth.interceptor";
-import {provideAnimations} from "@angular/platform-browser/animations";
-import {MessageService} from "primeng/api";
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimations(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
-    MessageService
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
-
--- TavolaFront/src/app/app.routes.ts
+++ TavolaFront/src/app/app.routes.ts
@@ -1,33 +1,3 @@
 import { Routes } from '@angular/router';
-import {authGuard} from "./guards/auth.guard";
 
-export const routes: Routes = [
-  {
-    path: "auth/login",
-    loadComponent: () => import("./pages/login/login.component").then(mod => mod.LoginComponent),
-  },
-  {
-    path: "auth/register",
-    loadComponent: () => import("./pages/register/register.component").then(mod => mod.RegisterComponent),
-  },
-  {
-    path: "cliente",
-    loadComponent: () => import("./pages/cliente/cliente.component").then(mod => mod.ClienteComponent),
-    canActivate: [authGuard]
-  },
-  {
-    path: "",
-    loadComponent: () => import("./pages/cliente/cliente.component").then(mod => mod.ClienteComponent),
-    canActivate: [authGuard]
-  },
-  {
-    path: "routeMap",
-    loadComponent: () => import("./pages/routeMap/routeMap.component").then(mod => mod.RouteMap),
-    canActivate: [authGuard]
-  },
-  {
-    path: "historico",
-    loadComponent: () => import("./pages/historico-rotas/historico-rotas.component").then(mod => mod.HistoricoRotasComponent),
-    canActivate: [authGuard]
-  }
-];
+export const routes: Routes = [];
