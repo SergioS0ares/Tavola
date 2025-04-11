1482e730dc0eed66da9d1d8346efcaa9cbea2dc8
Salahil <julioykawahata@gmail.com>
2025-04-11 19:48:12 -0300

    Requisições funcionando login e register

--- TavolaApp/src/main/java/TavolaSoftware/TavolaApp/config/SecurityConfig.java
+++ TavolaApp/src/main/java/TavolaSoftware/TavolaApp/config/SecurityConfig.java
@@ -1,31 +1,40 @@
 package TavolaSoftware.TavolaApp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import TavolaSoftware.TavolaApp.REST.security.JwtAuthenticationFilter;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    "/auth/login",
-                    "/auth/register",
-                    "/test/dev-token",
-                    "/v3/api-docs/**",
-                    "/swagger-ui/**",
-                    "/swagger-ui.html"
-                ).permitAll()
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	@Autowired
+	private JwtAuthenticationFilter jwtFilter;
 
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers(
+	                "/auth/login",
+	                "/auth/register",
+	                "/test/dev-token",
+	                "/v3/api-docs/**",
+	                "/swagger-ui/**",
+	                "/swagger-ui.html"
+	            ).permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+	    return http.build();
+	}
 }
