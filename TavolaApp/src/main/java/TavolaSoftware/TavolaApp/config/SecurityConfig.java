921c788dcebb9b8eac4fe0bc42640db1b1dca029
Salahil <julioykawahata@gmail.com>
2025-05-28 20:49:00 -0300

    Implementado DTOs de Restaurante e Cliente funcionais

--- TavolaApp/src/main/java/TavolaSoftware/TavolaApp/config/SecurityConfig.java
+++ TavolaApp/src/main/java/TavolaSoftware/TavolaApp/config/SecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import TavolaSoftware.TavolaApp.REST.security.JwtAuthenticationFilter;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +22,7 @@ public class SecurityConfig {
     private JwtAuthenticationFilter jwtFilter;
     
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
