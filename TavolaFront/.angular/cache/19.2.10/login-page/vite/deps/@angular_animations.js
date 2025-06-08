e33d26634e2809525b067608d7b884474226aa4a
SergioS0ares <sergyn200295@gmail.com>
2025-06-08 13:23:34 -0300

    build: Adiciona .angular/cache ao .gitignore e remove do tracking

---
+++ TavolaFront/.angular/cache/19.2.10/login-page/vite/deps/@angular_animations.js
@@ -0,0 +1,55 @@
+import {
+  AnimationBuilder,
+  AnimationFactory,
+  BrowserAnimationBuilder
+} from "./chunk-UEERIBQV.js";
+import {
+  AUTO_STYLE,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-WY2VMIPC.js";
+import "./chunk-GAMILAFO.js";
+import "./chunk-O3MZQZIU.js";
+import "./chunk-CR2THLZV.js";
+import "./chunk-3LZ7TQJT.js";
+import "./chunk-QCX4XGGK.js";
+import "./chunk-3SRVZXQZ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
